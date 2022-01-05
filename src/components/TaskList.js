bec487494f858a364eece7ab5df9884fc0fdabde
Fabrice Folituu <fabrice.folituu@lwane.com>
2022-01-05 10:01:32 -1000

    build: wire data with redux tools

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Task from "./Task";
+import { useDispatch, useSelector } from "react-redux";
+import { updateTaskState } from "../lib/store";
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   /** Checks if it's in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
@@ -12,11 +14,16 @@ TaskList.propTypes = {
   /** Event to change the task to archived */
   onArchiveTask: PropTypes.func,
 };
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false,
 };
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+export default function PureTaskList({
+  loading,
+  tasks,
+  onPinTask,
+  onArchiveTask,
+}) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -63,5 +70,33 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
+  );
+}
+
+/** Component With Redux */
+export function TaskList() {
+  // We're retrieving our state from the store
+  const tasks = useSelector((state) => state.tasks);
+  // We're defining an variable to handle dispatching the actions back to the store
+  const dispatch = useDispatch();
+
+  const pinTask = (value) => {
+    // We're dispatching the Pinned event back to our store
+    dispatch(updateTaskState({ id: value, newTaskState: "TASK_PINNED" }));
+  };
+  const archiveTask = (value) => {
+    // We're dispatching the Archive event back to our store
+    dispatch(updateTaskState({ id: value, newTaskState: "TASK_ARCHIVED" }));
+  };
+
+  const filteredTasks = tasks.filter(
+    (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+  );
+  return (
+    <PureTaskList
+      tasks={filteredTasks}
+      onPinTask={(task) => pinTask(task)}
+      onArchiveTask={(task) => archiveTask(task)}
+    />
   );
 }
