005d75489aa29938f35e219a66d6620d057a5a13
Fabrice Folituu <fabrice.folituu@lwane.com>
2022-01-05 08:59:04 -1000

    update: tasklist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
