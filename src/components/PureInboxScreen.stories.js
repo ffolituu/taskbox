59372eb7a621427207b3c96ba4a09d25dbedbc49
Fabrice Folituu <fabrice.folituu@lwane.com>
2022-01-05 11:12:09 -1000

    build: screen component-driven developement cdd

---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { PureInboxScreen } from "./InboxScreen";
+import { Provider } from "react-redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import * as TaskListStories from "./TaskList.stories";
+import { fireEvent, within } from "@storybook/testing-library";
+
+export default {
+  component: PureInboxScreen,
+  decorators: [(story) => <Provider store={Mockstore}>{story()}</Provider>],
+  title: "PureInboxScreen",
+};
+
+// A super-simple mock of a redux store
+const Mockstore = configureStore({
+  reducer: {
+    tasks: createSlice({
+      name: "tasks",
+      initialState: TaskListStories.Default.args.tasks,
+      reducers: {
+        updateTaskState: (state, action) => {
+          const { id, newTaskState } = action.payload;
+          const task = state.findIndex((task) => task.id === id);
+          if (task >= 0) {
+            state[task].state = newTaskState;
+          }
+        },
+      },
+    }).reducer,
+  },
+});
+
+const Template = (args) => <PureInboxScreen {...args} />;
+export const Default = Template.bind({});
+export const Error = Template.bind({});
+export const WithInteractions = Template.bind({});
+
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+};
+
+Error.args = {
+  error: "Something",
+};
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,17 +1,31 @@
 /* A simple redux store/actions/reducer implementation.
  * A true app would be more complex and separated into different files.
  */
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// Our new error field is configured here
+const AppStateSlice = createSlice({
+  name: "appState",
+  initialState: "",
+  reducers: {
+    updateAppState: (state, action) => {
+      return {
+        ...state,
+        isError: action.payload,
+      };
+    },
+  },
+});
 
 /*
  * The initial state of our store when the app loads.
  * Usually, you would fetch this from a server.
  */
 const defaultTasks = [
-  { id: '1', title: 'Something', state: 'TASK_INBOX' },
-  { id: '2', title: 'Something more', state: 'TASK_INBOX' },
-  { id: '3', title: 'Something else', state: 'TASK_INBOX' },
-  { id: '4', title: 'Something again', state: 'TASK_INBOX' },
+  { id: "1", title: "Something", state: "TASK_INBOX" },
+  { id: "2", title: "Something more", state: "TASK_INBOX" },
+  { id: "3", title: "Something else", state: "TASK_INBOX" },
+  { id: "4", title: "Something again", state: "TASK_INBOX" },
 ];
 
 /*
@@ -20,12 +34,12 @@ const defaultTasks = [
  * https://redux-toolkit.js.org/api/createSlice
  */
 const TasksSlice = createSlice({
-  name: 'tasks',
+  name: "tasks",
   initialState: defaultTasks,
   reducers: {
     updateTaskState: (state, action) => {
       const { id, newTaskState } = action.payload;
-      const task = state.findIndex(task => task.id === id);
+      const task = state.findIndex((task) => task.id === id);
       if (task >= 0) {
         state[task].state = newTaskState;
       }
@@ -35,6 +49,8 @@ const TasksSlice = createSlice({
 
 // The actions contained in the slice are exported for usage in our components
 export const { updateTaskState } = TasksSlice.actions;
+// The actions contained in the new slice are exported to be used in our components
+export const { updateAppState } = AppStateSlice.actions;
 
 /*
  * Our app's store configuration goes here.
@@ -44,6 +60,7 @@ export const { updateTaskState } = TasksSlice.actions;
 const store = configureStore({
   reducer: {
     tasks: TasksSlice.reducer,
+    isError: AppStateSlice.reducer,
   },
 });
 
