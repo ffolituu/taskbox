3e4fdff1b60eb34df86c2a044d12d2df35da6952
Fabrice Folituu <fabrice.folituu@lwane.com>
2022-01-05 09:23:52 -1000

    update: stories tasklist component

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,68 +1,51 @@
 import React from "react";
+
 import TaskList from "./TaskList";
+import * as TaskStories from "./Task.stories";
 
 export default {
   component: TaskList,
   title: "TaskList",
-  //   decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
+  decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
 };
 
 const Template = (args) => <TaskList {...args} />;
+
 export const Default = Template.bind({});
 export const WithPinnedTasks = Template.bind({});
 export const Loading = Template.bind({});
 export const Empty = Template.bind({});
 
 Default.args = {
-  loading: false,
+  // Shaping the stories through args composition.
+  // The data was inherited from the Default story in task.stories.js.
   tasks: [
-    {
-      id: "1",
-      title: "Tâche 1",
-      updatedAt: new Date(2021, 0, 1, 9, 0),
-    },
-    {
-      id: "2",
-      title: "Tâche 2",
-      updatedAt: new Date(2021, 0, 1, 9, 0),
-    },
-    {
-      id: "3",
-      title: "Tâche 3",
-      updatedAt: new Date(2021, 0, 1, 9, 0),
-    },
-    {
-      id: "4",
-      title: "Tâche 4",
-      updatedAt: new Date(2021, 0, 1, 9, 0),
-    },
-    {
-      id: "5",
-      title: "Tâche 5",
-      updatedAt: new Date(2021, 0, 1, 9, 0),
-    },
-    {
-      id: "6",
-      title: "Tâche 6",
-      updatedAt: new Date(2021, 0, 1, 9, 0),
-    },
+    { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
+    { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
+    { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
+    { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
+    { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
+    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
   ],
 };
 
 WithPinnedTasks.args = {
-  loading: false,
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Default story.
   tasks: [
     ...Default.args.tasks.slice(0, 5),
-    { id: "6", title: "Tâche 6 (épinglé)", state: "TASK_PINNED" },
+    { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
   ],
 };
 
 Loading.args = {
-  loading: true,
   tasks: [],
+  loading: true,
 };
 
 Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
+  ...Loading.args,
   loading: false,
-  tasks: [],
 };
